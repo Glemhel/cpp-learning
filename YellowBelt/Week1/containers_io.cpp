0aed61ef6be37efcb86f496ff7f236ac8f34b1a6
Mikhail Rudakov <ya.rudakov2001@yandex.ru>
2021-05-26 16:08:15 +0300

    Week 1 - Refactored Template Ouput

--- YellowBelt/Week1/containers_io.cpp
+++ YellowBelt/Week1/containers_io.cpp
@@ -4,39 +4,48 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 #include <map>
 #include <set>
 #include <string>
 
 using namespace std;
 
-
-// template vector output
-template<typename T>
-ostream &operator<<(ostream &ostream1, const vector<T> &v) {
-    for (const auto &i: v)
-        ostream1 << i << " ";
-    return ostream1;
+// template collection output to stream
+template<typename Collection>
+string Join(const Collection& c, char d){
+    stringstream ss;
+    bool first = true;
+    for (const auto& item: c){
+        if (!first)
+            ss << d;
+        ss << item;
+        first = false;
+    }
+    return ss.str();
 }
 
 // template pair output
 template<typename First, typename Second>
 ostream &operator<<(ostream &ostream1, const pair<First, Second> &v) {
-    ostream1 << v.first << "," << v.second;
-    return ostream1;
+    return ostream1 << '(' << v.first << "," << v.second << ')';
+}
+
+// template vector output
+template<typename T>
+ostream &operator<<(ostream &ostream1, const vector<T> &v) {
+    return ostream1 << "[" << Join(v, ',') << "]";
 }
 
 // template map output
 template<typename Key, typename Value>
 ostream &operator<<(ostream &ostream1, const map<Key, Value> &m) {
-    for (const auto &i: m)
-        ostream1 << i << " ";
-    return ostream1;
+    return ostream1 << "{" <<  Join(m, ',') << "}";
 }
 
 
 int main() {
-    vector<double> vi = {1, 2.4, 3};
+    vector<vector<int>> vi = {{1, 2}, {2, 3}, {4, 5}};
     cout << vi << endl;
     map<int, double> m = {{1, 2.5},
                           {3, 4}};
